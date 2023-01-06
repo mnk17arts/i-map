a9da0681128c5e0a8155fee128149031c938c8de
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2023-01-07 04:03:19 +0530

    Updates

---
+++ precache-manifest.88a5026950c3e1650b8d02cc6c9fad2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3429383c6f8d92f511",
+    "url": "/i-map/static/css/main.240a36bd.chunk.css"
+  },
+  {
+    "revision": "0e3429383c6f8d92f511",
+    "url": "/i-map/static/js/main.ab703361.chunk.js"
+  },
+  {
+    "revision": "515f8adb017007d6ce26",
+    "url": "/i-map/static/js/runtime~main.16e338e5.js"
+  },
+  {
+    "revision": "9ef879bb9f4ede410c27",
+    "url": "/i-map/static/js/2.16244d63.chunk.js"
+  },
+  {
+    "revision": "c402d1c83f30c7ffef7a3dfa730d629e",
+    "url": "/i-map/index.html"
+  }
+];
